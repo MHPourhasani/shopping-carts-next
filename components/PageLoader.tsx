980898bea1d59532d5b73c8662330fdfa6ec55e9
m.pourhasani <mhpourhasani.dev@gmail.com>
2024-11-12 11:45:56 +0330

    improvement page loader ui

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -3,9 +3,9 @@ import logo from "@/public/icons/android/android-launchericon-512-512.png";
 
 const PageLoader = () => {
     return (
-        <div className="flex h-screen w-screen flex-col items-center justify-center gap-20">
-            <Image src={logo} alt="logo" className="rounded-2xl" width={150} height={150} />
-            <span className="loader"></span>;
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-20">
+            <Image src={logo} alt="logo" width={150} height={150} className="size-28 rounded-xl lg:size-40 lg:rounded-2xl" />
+            <span className="loader"></span>
         </div>
     );
 };
