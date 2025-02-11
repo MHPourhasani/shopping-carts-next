306384d941dfdf7fe6cebc3ff6d38d442d831e1d
MHPourhasani <mhpourhasani.dev@gmail.com>
2025-02-11 23:35:39 +0330

    update robots file

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: "/",
             disallow: ["/dashboard", "/dashboard/*", "/profile", "/profile/*"],
         },
-        sitemap: "https://mhp-shop.vercel.app",
+        sitemap: "https://mhp-shop.vercel.app/sitemap.xml",
     };
 }
